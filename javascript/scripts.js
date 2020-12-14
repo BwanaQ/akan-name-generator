6ed36bb1ead0e23d8a5d3f49ebfcd07dfd4223b2
BwanaQ <thunjawax@gmail.com>
2020-12-14 09:56:09 +0300

    remove gender error

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -13,6 +13,8 @@ var getAkanName = function(){
   }
  
   var dateOfBirth = document.getElementById("dateOfBirth").value;
+  console.log(gender +" "+ dateOfBirth);
+
   //slice to get day month century year
   var dd = parseInt(dateOfBirth.slice(0,2));
   var mm = parseInt(dateOfBirth.slice(3,5));
@@ -20,16 +22,14 @@ var getAkanName = function(){
   var yy = parseInt(dateOfBirth.slice(8,10));
 
   //validation
-  if (gender===''||dateOfBirth===''){
-    alert("All fields are required!");
-  }
+
   if (gender==='M'||gender==='F'){
     if (mm>0 && mm<=12){
       if(dd>0 && dd<=31){
         dayOfWeekRHS = (((cc/4) -2*cc-1)+((5*yy/4))+((26*(mm+1)/10))+dd);
         dayOfWeekRHSFloor = Math.floor(dayOfWeekRHS);
         dayOfWeek = dayOfWeekRHSFloor % 7;
-        if (genderUp==='M'){
+        if (gender==='M'){
           return(alert("Your male Akan name is "+ maleNames[dayOfWeek]));
         } else {
           return(alert("Your female Akan name is "+ femaleNames[dayOfWeek]));
